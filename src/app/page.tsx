6ace08c745a708e12ec9689b19216614399ab606
X-DSI <xmandsi@gmail.com>
2024-02-08 12:41:37 +0530

    Updated fonts and Texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 export default function Home() {
   return (
     <main className="h-screen bg-gradient-to-r from-superlight-lavender from-20% via-lavender via-60% md:via-50% to-dark-lavender to-100% ">
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div></div>
         <div className="flex">
           <div className="font-niconne text-[102px]">Danny</div>
-          <div className="text-[102px]">&</div>
-          <div className="text-[102px]">Abby</div>
+          <div className="font-inika text-[102px] text-[#5B6345]">&</div>
+          <div className="font-niconne text-[102px]">Abby</div>
+        </div>
+        <div className="text-[48px] font-thin pb-6">MARCH 10, 2024</div>
+        <div className="rounded-md border-2 border-[#5B6345] text-[#5B6345] text-2xl p-2 font-semibold">
+          More details soon...
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "superlight-lavender": "#E6D3ED",
-      lavender: "#D68FEF",
-      "dark-lavender": "#C034F2",
+    extend: {
+      fontFamily: {
+        niconne: ["Niconne, cursive", "sans-serif"],
+        inika: ["Inika, serif", "sans-serif"],
+        poppinsThinItalic: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "superlight-lavender": "#E6D3ED",
+        lavender: "#D68FEF",
+        "dark-lavender": "#C034F2",
+      },
     },
   },
   plugins: [],
