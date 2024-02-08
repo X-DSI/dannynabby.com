77ac1646cae699bc70b8318bd13c3ebc8482ddf2
X-DSI <xmandsi@gmail.com>
2024-02-08 11:21:18 +0530

    Changes to Font- family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="h-screen bg-gradient-to-r from-superlight-lavender from-20% via-lavender via-60% md:via-50% to-dark-lavender to-100% ">
       <div className="flex items-center justify-center h-screen">
         <div className="flex">
-          <div className="font-niconne text-lg">Danny</div>
-          <div>&</div>
-          <div className="">Abby</div>
+          <div className="font-niconne text-[102px]">Danny</div>
+          <div className="text-[102px]">&</div>
+          <div className="text-[102px]">Abby</div>
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
     colors: {
       "superlight-lavender": "#E6D3ED",
       lavender: "#D68FEF",
       "dark-lavender": "#C034F2",
-    },
-    fontFamily: {
-      niconne: ["Niconne", "sans-serif"],
     },
   },
   plugins: [],
