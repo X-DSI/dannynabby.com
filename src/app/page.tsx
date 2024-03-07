bc13112cb0790f62da972b64634335ea2953a9be
X-DSI <xmandsi@gmail.com>
2024-03-07 11:18:08 +0530

    Live stream link and location updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Button from "../components/Button";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen min-w-screen bg-gradient-to-r from-superlight-lavender from-20% via-lavender via-60% to-dark-lavender to-100% ">
@@ -14,17 +16,29 @@ export default function Home() {
         </div>
         <div className="flex gap-4">
           <div className="font-niconne text-[48px] md:text-[102px]">Danny </div>
-          <div className="font-inika text-[48px] md:text-[102px] text-[#5B6345]">
-            {" "}
-            &{" "}
-          </div>
+          <div className="font-inika text-[48px] md:text-[102px]"> & </div>
           <div className="font-niconne text-[48px] md:text-[102px]"> Abby</div>
         </div>
         <div className="text-[24px] md:text-[48px] font-thin pb-6">
           MARCH 10, 2024
         </div>
-        <div className="rounded-md border-2 border-[#5B6345] text-[#5B6345] text-[15px] md:text-2xl p-2 font-semibold">
-          More details soon...
+        <div className="flex gap-3">
+          <div className="rounded-md border-2 border-[#5B6345] text-[#5B6345] text-[15px] md:text-2xl p-2 font-semibold text-center">
+            <Link
+              isPrimary={true}
+              href="https://maps.app.goo.gl/qviaTXu6zU6pnwk98"
+            >
+              Location
+            </Link>
+          </div>
+          <div className="rounded-md border-2 border-[#5B6345] text-[#5B6345] text-[15px] md:text-2xl p-2 font-semibold text-center">
+            <Link
+              isPrimary={true}
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            >
+              Live Stream
+            </Link>
+          </div>
         </div>
         <div className="visible md:hidden">
           <Image
